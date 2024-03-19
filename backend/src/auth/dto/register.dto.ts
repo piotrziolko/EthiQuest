4522f7c18463f879bd91e56931bcd5ace40ec4b2
Piotr <ziolko.piotr@outlook.com>
2024-03-19 20:00:18 +0100

    fix: email validator

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class RegisterDto {
   @ApiProperty()
@@ -8,6 +8,7 @@ export class RegisterDto {
 
   @ApiProperty()
   @IsNotEmpty()
+  @IsEmail()
   email!: string;
 
   @ApiProperty()
