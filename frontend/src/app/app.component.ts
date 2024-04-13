a01328e538234e13ad92872d381da229de670a56
Piotr <ziolko.piotr@outlook.com>
2024-04-13 08:04:02 +0200

    feat(front): navbar set link to login page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent {
         : [
             {
               name: 'Login',
-              routerLink: '/login',
+              routerLink: '/auth/login',
             },
           ]),
       ...(this.isLogged
