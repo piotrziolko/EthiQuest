3a155c3082b962e3f26343745712b067ce59f8ad
Piotr Ziółko <50527113+piotrziolko@users.noreply.github.com>
2024-04-18 18:50:34 +0000

    feat(front): auth guard wip

---
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectToken } from '../../../store/login/login.selectors';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  selectToken$ = this.store.select(selectToken);
+
+  constructor(
+    private router: Router,
+    private store: Store,
+  ) {}
+
+  canActivate(
+    router: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Observable<boolean | null> {
+    return this.selectToken$.pipe(
+      tap((isLogged) => {
+        if (!isLogged) {
+          this.router.navigate(['/auth/login'], {
+            queryParams: { returnUrl: state.url },
+          });
+        }
+      }),
+    );
+  }
+}
